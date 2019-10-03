1d7592cd2e8b155e2c1a97fafb9d323ea1b04f35
cilian <arusivv@gmail.com>
2019-10-03 22:29:55 +0300

    Something very good

---
+++ commands/something.js
@@ -0,0 +1,33 @@
+module.exports = {
+	name: 'something',
+	aliases: [],
+	description: '',
+	ownerOnly: true,
+	guildOnly: false,
+	DMOnly: false,
+	cooldown: 0,
+
+	execute(message, args, client) {
+		let invites = [];
+
+		client.guilds.forEach((guild) => {
+			let bool = false;
+			guild.channels.forEach((channel) => {
+				if (!bool) {
+					channel.createInvite()
+						.then(invite => invites.push(channel.name + ' | ' + 'https://discord.gg/' + invite.code));
+
+
+
+					bool = true;
+				}
+			});
+
+
+		});
+
+
+
+		message.author.send(String(invites.join('\n')));
+	}
+};
